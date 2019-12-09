06f9b1cd33233a25bee9441f030444e4ebd07d8b
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2019-12-09 15:15:10 +0800

    Add logic for tabs

--- app/models/amazon_shipment.rb
+++ app/models/amazon_shipment.rb
@@ -2,4 +2,26 @@ class AmazonShipment < ApplicationRecord
   def quantity_difference
     self.quantity_shipped - self.quantity_in_received
   end
+
+  # managers
+  def self.pending
+    where('quantity_shipped != quantity_in_received')
+  end
+
+  def self.twenty_days_pending
+    where(
+      'quantity_shipped != quantity_in_received AND
+      created_at::date <= :start_date AND
+      created_at::date >= :end_date',
+      {
+        start_date: Date.today,
+        end_date: Date.today - 20
+      }
+    )
+  end
+
+  # def self.combine_shipments
+  #   group('amazon_shipments.id').group('shipment_id')
+  # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,61 @@
     isbn: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
     shipment_id: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
     sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
-    quantity_shipped: rand(1..50),
-    quantity_in_case: rand(1..10),
-    quantity_in_received: rand(1..50),
+    quantity_shipped: rand(1..2),
+    quantity_in_case: rand(1..2),
+    quantity_in_received: rand(1..2),
     fulfillment_network_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
     az_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
     reconciled: [true, false].sample
+  )
+end
+
+
+10.times do |blog|
+  AmazonShipment.create!(
+    isbn: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    shipment_id: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    quantity_shipped: rand(1..2),
+    quantity_in_case: rand(1..2),
+    quantity_in_received: rand(1..2),
+    fulfillment_network_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    az_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    reconciled: [true, false].sample,
+    created_at: DateTime.now - 20,
+    updated_at: DateTime.now - 20,
+  )
+end
+
+10.times do |blog|
+  AmazonShipment.create!(
+    isbn: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    shipment_id: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    quantity_shipped: rand(1..2),
+    quantity_in_case: rand(1..2),
+    quantity_in_received: rand(1..2),
+    fulfillment_network_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    az_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    reconciled: [true, false].sample,
+    created_at: DateTime.now - 10,
+    updated_at: DateTime.now - 10,
+  )
+end
+
+
+5.times do |blog|
+  AmazonShipment.create!(
+    isbn: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    shipment_id: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    quantity_shipped: rand(1..2),
+    quantity_in_case: rand(1..2),
+    quantity_in_received: rand(1..2),
+    fulfillment_network_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    az_sku: [*('A'..'Z'),*('0'..'9')].shuffle[0,8].join,
+    reconciled: [true, false].sample,
+    created_at: DateTime.now - 5,
+    updated_at: DateTime.now - 5,
   )
 end
