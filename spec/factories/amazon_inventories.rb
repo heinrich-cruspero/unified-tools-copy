96c14aaeeed60b298c13dc172e57fc14ec44f4ab
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2020-05-05 02:26:01 +0800

    Add model specs for amazon_inventories

--- spec/factories/amazon_inventories.rb
+++ spec/factories/amazon_inventories.rb
@@ -1,5 +1,11 @@
+require 'faker'
+
 FactoryBot.define do
   factory :amazon_inventory do
-    
+    isbn { Faker::Alphanumeric.alphanumeric(number: 10) }
+    condition { Faker::Alphanumeric.alphanumeric(number: 10) }
+    fnsku { Faker::Alphanumeric.alphanumeric(number: 10) }
+    in_stock_supply_quantity { Faker::Number.number(digits: 1) }
+    inbound_quantity { Faker::Number.number(digits: 1) }
   end
 end
--- spec/models/amazon_inventory_spec.rb
+++ spec/models/amazon_inventory_spec.rb
@@ -1,5 +1,32 @@
+require 'faker'
 require 'rails_helper'
 
 RSpec.describe AmazonInventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  valid_attributes = {
+    isbn: Faker::Alphanumeric.alphanumeric(number: 10),
+    condition: Faker::Alphanumeric.alphanumeric(number: 10),
+    fnsku: Faker::Alphanumeric.alphanumeric(number: 10),
+    in_stock_supply_quantity: Faker::Number.number(digits: 1),
+    inbound_quantity: Faker::Number.number(digits: 1),
+  }
+
+  context 'AmazonInventory Test Cases' do
+    it 'adds valid entry' do
+      amazon_inventory = AmazonInventory.create! valid_attributes
+      expect(AmazonInventory.last).to eq(amazon_inventory)
+    end
+
+    it 'raises NotNullViolation on isbn nil' do
+      expect{FactoryBot.create(:amazon_inventory, isbn: nil)}.to raise_error ActiveRecord::NotNullViolation
+    end
+
+    it 'raises NotNullViolation on condition nil' do
+      expect{FactoryBot.create(:amazon_inventory, condition: nil)}.to raise_error ActiveRecord::NotNullViolation
+    end
+
+    it 'raises NotNullViolation on fnsku nil' do
+      expect{FactoryBot.create(:amazon_inventory, fnsku: nil)}.to raise_error ActiveRecord::NotNullViolation
+    end
+
+  end
 end
