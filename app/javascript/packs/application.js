a635066c8d4f0ab31d682fe25d896b533be57ca4
Karim Elnadim <karim@rev365.com>
2020-05-11 11:36:00 -0500

    readd fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,12 +13,13 @@ require("bootstrap-table");
 require("bootstrap-daterangepicker");
 require("moment");
 require("bootstrap-daterangepicker");
+require("@fortawesome/fontawesome-free");
 
 require("../custom/sidebar")
 require("../custom/amazon_table")
 require("../custom/indaba_sku_tab")
 
-
+import "@fortawesome/fontawesome-free/css/all.css";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
