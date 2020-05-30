e5655e9be2f15694088c637e62d457dee3dcd6f4
Richa <rrathore@rev365.com>
2020-05-30 15:35:08 +0530

    Added specs

--- spec/models/amazon_order_item_spec.rb
+++ spec/models/amazon_order_item_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe AmazonOrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:amazon_order_item) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:amazon_order) }
+  end  
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,13 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
 
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
