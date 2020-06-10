5b246f575244099fbfa17932cedb94f2cdcf68c4
Richa <rrathore@rev365.com>
2020-06-10 19:26:30 +0530

    Added new columns

--- db/migrate/20200520184826_create_amazon_orders.rb
+++ db/migrate/20200520184826_create_amazon_orders.rb
@@ -5,6 +5,9 @@ class CreateAmazonOrders < ActiveRecord::Migration[6.0]
       t.datetime :purchase_date, null: false
       t.text :status, null: false, index: true
       t.text :amazon_order_id, null: false
+      t.text :state
+      t.text :city
+      t.text :pin
       t.index [:amazon_order_id], unique: true
       t.timestamps
     end
