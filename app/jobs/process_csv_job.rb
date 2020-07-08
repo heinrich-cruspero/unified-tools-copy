7cc229c2a17d5268b3d8fe6f64e308ed5cd0b81a
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2020-07-09 00:35:28 +0800

    Add destroy job on 3 tries. missing way on how to notify user for failed job

--- app/jobs/process_csv_job.rb
+++ app/jobs/process_csv_job.rb
@@ -8,4 +8,12 @@ class ProcessCsvJob < ApplicationJob
   def perform(*args)
     process_csv(args[0], args[1])
   end
+
+  def max_attempts
+    3
+  end
+
+  def destroy_failed_jobs?
+    true
+  end
 end
