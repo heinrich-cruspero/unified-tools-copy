a04eafe18e78cd26c899fbbd3cfea47bcd0c0be5
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2020-09-24 17:43:51 +0800

    updated table fields source, get years, oe link

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,4 +7,29 @@ class Book < ApplicationRecord
         using: {
             :tsearch => {:prefix => true}
         }
+
+    def oe_isbn_sales_rank
+        @oe_book = Book.where(oe_isbn: self.oe_isbn).take
+        @oe_book.sales_rank
+    end
+
+    def self.get_jan_year
+        month = Date.today.month
+        year = Date.today.strftime("%y")
+        prev_year = (Date.today - 1.year).strftime("%y")
+        if month > 1
+            return year
+        end
+        prev_year
+    end
+
+    def self.get_aug_year
+        month = Date.today.month
+        year = Date.today.strftime("%y")
+        prev_year = (Date.today - 1.year).strftime("%y")
+        if month > 8
+            return year
+        end
+        prev_year
+    end
 end
