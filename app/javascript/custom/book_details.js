276fb853db445011c6f5947c1edec8b35dbff90b
Marc Heinrich Cruspero <heinrich.cruspero@gmail.com>
2020-10-13 12:14:08 +0800

    use flex for detail content

--- app/javascript/custom/book_details.js
+++ app/javascript/custom/book_details.js
@@ -3,15 +3,6 @@ $( document ).on('turbolinks:load', function() {
         searchBook();
     });
 
-    var book_obj = document.getElementById("book-record")
-    if (!book_obj){
-        $("#book-details-main").removeClass("book-details-table");
-        $("#book-details-main").attr("id", "");
-        $("#book-details-amazon-inventory").removeClass("book-details-table");
-        $("#book-details-amazon-inventory").attr("id", "");
-    }
-
-
     function searchBook(){
         var book_val = $('#search_book').val()
         if (book_val.length == 10 || book_val.length == 13) {
