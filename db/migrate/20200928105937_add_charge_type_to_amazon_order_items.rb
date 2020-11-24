7b7703410eaea5244ff8b95c8f299152c7abbd49
Karim Elnadim <karim@rev365.com>
2020-11-24 13:17:01 -0600

    update schema

--- db/migrate/20200928105937_add_charge_type_to_amazon_order_items.rb
+++ db/migrate/20200928105937_add_charge_type_to_amazon_order_items.rb
@@ -1,6 +1,6 @@
 class AddChargeTypeToAmazonOrderItems < ActiveRecord::Migration[6.0]
   def change
     add_column :amazon_order_items, :charge_type, :string
-    add_index :amazon_order_items, :charge_type, unique: true
+    add_index :amazon_order_items, :charge_type
   end
 end
