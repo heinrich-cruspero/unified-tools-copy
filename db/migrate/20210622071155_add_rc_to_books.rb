e9a1641dbe3347390fdfae0d526e9fff3e56b73d
Karim Elnadim <karim@rev365.com>
2021-06-23 15:01:11 -0500

    schema update

--- db/migrate/20210622071155_add_rc_to_books.rb
+++ db/migrate/20210622071155_add_rc_to_books.rb
@@ -1,8 +1,8 @@
 class AddRcToBooks < ActiveRecord::Migration[6.0]
   def change
-    add_column :books, :rc, :int, default: 0, null: false
-    add_column :books, :monthly_rc, :int, default: 0, null: false
-    add_column :books, :one_eighty_rc, :int, default: 0, null: false
+    add_column :books, :rc, :integer, default: 0, null: false
+    add_column :books, :monthly_rc, :integer, default: 0, null: false
+    add_column :books, :one_eighty_rc, :integer, default: 0, null: false
     add_index :books, :rc
     add_index :books, :monthly_rc
     add_index :books, :one_eighty_rc
